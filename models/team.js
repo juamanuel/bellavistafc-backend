c7193db223f2cb884249c8f848296d2dc17073d5
Juan Manuel Lopez <juanlg@me.com>
2018-11-11 07:52:10 -0700

    Create Team Scheme

--- models/team.js
+++ models/team.js
@@ -3,8 +3,35 @@ const { v4 } = require('uuid')
 
 const teamSchema = new mongoose.Schema({
     uuid: { type: String, default: v4 },
-    name: { type: String, required: true },
-    shortName: {type: String, required: true},
+    teamName: { type: String, required: true, lowercase: true },
+    shortTeamName: {type: String, required: true},
+    logo:{type: String},
+    group:{type:Number},
+    competition:{
+      idCompetition:{type:Number},
+      competitionName:{type:String, required:true},
+      currentRound:{type:Number},
+      totalGroup:{type:Number}
+    },
+    players:{
+      playerName:{type:String, required:true,lowercase:true},
+      lastName:{type:String,required:true,lowercase:true},
+      shirtNumber:{type:String},
+      position:{type:String},
+      yellowCards:{type:Number},
+      redCards:{type:Number},
+      goals:{type:Number}
+    },
+    stadistics:{
+      points:{type:Number},
+      extraPoints:{type:Number},
+      gf:{type:Number},
+      gc:{type:Number},
+      win:{type:Number},
+      draw:{type:Number},
+      lose:{type:Number},
+      diff:{type:Number}
+    }
    },{
     // overrides default collection name auto created
     collection: 'teams'
